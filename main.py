880a978fdbbf3c08dfb49ab60d1fd168aa4667e3
Caleb Jhones <calebjhones@gmail.com>
2018-01-30 14:30:00 -0700

    Added some documentation comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import time # for time.sleep
 basic_land_cards = ["plains", "island", "swamp", "mountain", "forest", "wastes"]
 validate = True
 
+# Enumerate the types of decks available
 class DeckFormat(Enum):
     Standard = 1
     Modern = 2
@@ -46,7 +47,7 @@ class Deck:
         if validate:
             http_obj = http_processing()
             if not http_obj.validate_card(card):
-                print("ERROR: {} is not a real card!".format(card.title())) # FIXME
+                print("ERROR: {} is not a real card!".format(card.title()))
                 print(card.title())
                 return False
 
@@ -95,6 +96,7 @@ class Deck:
         fname = self.deck_format.name + '-' + self.deck_name.title().replace(' ', '-')
         image_list = list()
         counter = 0
+        # writing a .tex file by hand
         with open(fname + '.tex', 'w') as f:
             f.write('\\documentclass[11pt] {article}\n')
             f.write('\\usepackage[margin=0.3in]{geometry}\n')
@@ -116,28 +118,43 @@ class Deck:
             f.write('\\end{landscape}\n')
             f.write('\\end{document}\n')
 
-        # if the os call executes with a return status of 0
+        # compile the .tex file, throwing the output to /dev/null for cleanliness
         if not os.system('pdflatex -halt-on-error -interaction=nonstopmode ' + fname + ' >> /dev/null'):
+            # if the os call executes with a return status of 0
             print("PDF created")
         else:
+            # otherwise the os tells us something didn't go well
             print("Something broke... No output file created")
 
         # clean up all of the images we downloaded
         for img in image_list:
             os.system('rm ' + img)
 
-        os.system('rm {}.aux {}.log'.format(fname, fname)) # clean up auxilary files
-        os.system('rm {}.tex'.format(fname)) # clean up the tex file
+        # clean up the .tex files that aren't really needed anymore
+        os.system('rm {}.aux {}.log'.format(fname, fname))
+        os.system('rm {}.tex'.format(fname))
 
     def load_csv(self, path, index):
+        ''' Load a decklist from a csv file.
+
+            INPUT:
+                path: the filepath to the .csv file
+
+            OUTPUT:
+                none
+
+            TODO:
+                only accepts one copy of each card at the moment!
+        '''
         index = int(index)
         with open(path, 'r') as f:
             try:
                 for line in f:
-                    self.add_card(1, line.strip())
+                    self.add_card(1, line.strip()) #TODO
             except:
                 print("There was an unexpected error processing the file")
                 return
+        # immediately create proxies
         self.save_deck_proxies()
 
 class http_processing:
@@ -146,19 +163,23 @@ class http_processing:
 
     def validate_card(self, card):
         ''' Uses scryfall.com's API to validate a card name. '''
+        # try to get info on the card from scryfall
         card_info = requests.get(self.scryfall_url + card)
+        # if we were successful, the http code will be 200
         if card_info.status_code == 200:
             c = card_info.json()['data']
+            # try to find a card name in scryfall's json glob that matches the one we're trying to add
             for i in range(len(c)):
                 if self.__clean_name(c[i]['name']) == self.__clean_name(card):
                     return True
+        # if it wasn't 200, return False
         return False
 
     def get_card_image(self, card):
         ''' Uses scryfall to pull a png image of a card for printing later.
 
             INPUT:
-                card, the card name we want to download
+                card: the card name we want to download
 
             OUTPUT:
                 the system path to the downloaded image
@@ -172,26 +193,30 @@ class http_processing:
                     img_url = c[i]['image_uris']['large']
                 else:
                     continue
+        time.sleep(0.005) # don't let scryfall lock us out for downloading too much too fast
+        # create a file path string to download to
         path = "./images/" + self.__clean_name(card) + ".jpg"
-        time.sleep(0.005) # don't let scryfall lock us out
+        # actually download/write the image file
         img_file = open(path, 'wb')
         img_file.write(requests.get(img_url).content)
         img_file.close()
+        # return the file path to the card image, so that we can use it later
         return path
 
     def __clean_name(self, s):
         ''' Strips annoying characters from a card name.
 
             INPUT:
-                s, the card name string to clean
+                s: the card name string to clean
 
             OUTPUT:
-                The card name, without any of the special chars
+                The card name without any of the special chars
         '''
         return s.lower().replace(' ', '_').replace(',','').replace("'", "").replace("/","")
 
 def create_deck():
     ''' Create and run a REPL to read card names and quantities from the user.'''
+
     # ask the user for a deck name
     my_name = input("Please give a name for your deck: ")
 
@@ -223,13 +248,17 @@ def create_deck():
         if input_tmp == "QUIT" or input_tmp == "Q" or input_tmp == "":
             break
 
+        # split the list to get to the number of cards at the initial position
         input_lst = input_tmp.split()
         try:
+            # add the right number of cards to the list
             number = int(input_lst[0])
         except:
+            # if there wasn't a number at the beginning, add one to the list
             number = 1
             input_lst.insert(0, 1)
 
+        # reconstruct the full card name after splitting it above
         card = ""
         for c in input_lst[1:]:
             card += c
